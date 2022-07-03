ced4b9383130d584a6c0ce769bda544ad9eeed66
peter <pz@thefuturerocks.com>
2022-07-03 10:06:21 +0800

    modal

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import { thumbnailBaseUrl } from '../constants/movie'
 import { Movie } from '../typings'
 
@@ -7,8 +9,16 @@ interface Props {
 }
 
 const Thumbnail = ({ movie }: Props) => {
+	const [modal, setModal] = useRecoilState(modalState)
+	const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
 	return (
-		<div className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'>
+		<div
+			onClick={() => {
+				setModal(true)
+				setCurrentMovie(movie)
+			}}
+			className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'
+		>
 			<Image
 				src={`${thumbnailBaseUrl}${movie.backdrop_path || movie.poster_path}`}
 				className='rounded-sm object-cover md:rounded'
