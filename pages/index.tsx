ebcbf9b72d36650530110f87db6ba6bde9a13979
peter <pz@thefuturerocks.com>
2022-07-03 10:12:54 +0800

    modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,11 +35,14 @@ const Home: NextPage<Props> = ({
 	console.log('environment:', process.env.NODE_ENV)
 	const { loading } = useAuth()
 	const showModal = useRecoilValue(modalState)
-	console.log(showModal)
 
 	if (loading) return null
 	return (
-		<div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+		<div
+			className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+				showModal && '!h-screen overflow-hidden'
+			}`}
+		>
 			<Head>
 				<title>TailWind</title>
 				<link rel='icon' href='/favicon.png' />
